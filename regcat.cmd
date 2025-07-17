670769e1b3bfecabc830b3b668d2a19d0cb24669
Ziv Kedem <atxcorp101@gmail.com>
2025-07-17 14:48:08 -0400

    Bug fixes
    - Fixed line ending bug

--- regcat.cmd
+++ regcat.cmd
@@ -17,7 +17,7 @@ IF ERRORLEVEL 1 ( :: Command line mode
 		ECHO. 1>&2
 		ECHO     file1.reg    An input file. 1>&2
 		ECHO     /O:filename  Output to filename. 1>&2
-		ECHO     /A           Add "Concatenate" to File Explorer context menu for .reg files. 1>&2
+		ECHO     /A           Add "Concatenate" to File Explorer context menu. 1>&2
 		ECHO     /R           Remove "Concatenate" from File Explorer context menu. 1>&2
 	) ELSE IF "%~1" == "/R" ( :: Remove from context menu
 		reg DELETE HKCU\Software\Classes\regfile\shell\Concatenate /f 1>NUL 2>NUL
@@ -60,14 +60,14 @@ GOTO :EOF
 			var False = 0;
 			var True = 1;
 			(function() {
-				var chars = app.commandLine.replace(/^ +| +$/g, '').split('');
+				var chars = app.commandLine.replace(/^ +| +$/g, "").split("");
 				var cutArg = true;
-				window.argv = [''];
+				window.argv = [""];
 				for (var i = 0; i < chars.length; i++) {
-					if (chars[i] == ' ' && cutArg) {
-						argv.push('');
+					if (chars[i] == " " && cutArg) {
+						argv.push("");
 					} else {
-						if (chars[i] == '"') {
+						if (chars[i] == "\"") {
 							cutArg = !cutArg;
 						}
 						argv[argv.length - 1] += chars[i];
@@ -77,46 +77,46 @@ GOTO :EOF
 			onerror = function(message) {
 				alert(message);
 				close();
-			}
+			};
 			onload = function() {
 				var ForReading = 1;
 				var TristateUseDefault = -2;
-				var fso = new ActiveXObject('Scripting.FileSystemObject');
+				var fso = new ActiveXObject("Scripting.FileSystemObject");
 				var UUIDRequested = false;
 				var seconds = 0;
-				var preamble = 'Windows Registry Editor Version 5.00';
-				var content = preamble + '\r\n';
+				var preamble = "Windows Registry Editor Version 5.00";
+				var content = preamble + "\r\n";
 				var directory = null;
 				var eraseWhenDone = false;
-				var extension = '';
+				var extension = "";
 				var fileList = [];
 				function generateUUID() {
-					var tl = new ActiveXObject('Scriptlet.TypeLib');
-					fso.GetStandardStream(1).WriteLine(tl.GUID.replace(/[{}]/g, ''));
+					var tl = new ActiveXObject("Scriptlet.TypeLib");
+					fso.GetStandardStream(1).WriteLine(tl.GUID.replace(/[{}]/g, ""));
 					close();
 				}
 				function zeroPad(n, size) {
 					if (size == undefined) {
 						size = 2;
 					}
-					return new Array(Math.max(0, size - n.toString().length + 1)).join('0') + n;
+					return new Array(Math.max(0, size - n.toString().length + 1)).join("0") + n;
 				}
 				function xmlTime(d) {
-					var timeString = '';
-					timeString += zeroPad(d.getFullYear(), 4) + '-';
-					timeString += zeroPad(d.getMonth() + 1) + '-';
-					timeString += zeroPad(d.getDate()) + 'T';
-					timeString += zeroPad(d.getHours()) + ':';
-					timeString += zeroPad(d.getMinutes()) + ':';
+					var timeString = "";
+					timeString += zeroPad(d.getFullYear(), 4) + "-";
+					timeString += zeroPad(d.getMonth() + 1) + "-";
+					timeString += zeroPad(d.getDate()) + "T";
+					timeString += zeroPad(d.getHours()) + ":";
+					timeString += zeroPad(d.getMinutes()) + ":";
 					timeString += zeroPad(d.getSeconds());
 					return timeString;
 				}
 				function scheduleAsTask() {
 					var TriggerTypeTime = 1;
 					var ActionTypeExec = 0;
-					var service = new ActiveXObject('Schedule.Service');
+					var service = new ActiveXObject("Schedule.Service");
 					service.Connect();
-					var rootFolder = service.GetFolder('\\');
+					var rootFolder = service.GetFolder("\\");
 					var taskDefinition = service.NewTask(0);
 					var trigger = taskDefinition.Triggers.Create(TriggerTypeTime);
 					var startTime = Date.now() + seconds * 1000;
@@ -125,39 +125,41 @@ GOTO :EOF
 					taskDefinition.Settings.Enabled = True;
 					taskDefinition.Settings.StartWhenAvailable = True;
 					taskDefinition.Settings.Hidden = False;
-					taskDefinition.Settings.DeleteExpiredTaskAfter = 'PT0S';
+					taskDefinition.Settings.DeleteExpiredTaskAfter = "PT0S";
 					trigger.StartBoundary = xmlTime(new Date(startTime));
 					trigger.EndBoundary = xmlTime(new Date(startTime + 1000));
-					trigger.Id = 'TimeTriggerId';
+					trigger.Id = "TimeTriggerId";
 					trigger.Enabled = True;
-					action.Path = '%COMSPEC%';
-					action.Arguments = '/C START "" mshta.exe ' + argv[0] + ' ';
+					action.Path = "%COMSPEC%";
+					action.Arguments = "/C START \"\" mshta.exe " + argv[0];
 					for (var i = 1; i < argv.length; i++) {
 						var argument = argv[i];
-						if (argument.substring(0, '/S:'.length) != '/S:') {
-							action.Arguments += ' ' + argument;
+						if (argument.substring(0, "/S:".length) != "/S:") {
+							action.Arguments += " " + argument;
 						}
 					}
-					rootFolder.RegisterTaskDefinition(fso.GetBaseName(argv[0]), taskDefinition, 6, '', '', 3);
+					rootFolder.RegisterTaskDefinition(fso.GetBaseName(argv[0]), taskDefinition, 6, "", "", 3);
 					close();
 				}
 				function checkLength(a) {
 					if (a.length < 1) {
-						throw new Error('You must specify at least one file.');
+						throw new Error("You must specify at least one file.");
 					}
 				}
 				function consumeFile(fileHandle) {
-					while (!fileHandle.AtEndOfStream) {
-						var line = fileHandle.ReadLine();
-						if (line != preamble) {
-							content += line + '\r\n';
-						}
+					var text = fileHandle.ReadAll();
+					if (text.search("\r") == -1 || text.search("\n") == -1) {
+						text = text.replace(/[\r\n]/g, "\r\n");
 					}
+					if (text.substring(text.length - 2) != "\r\n") {
+						text += "\r\n";
+					}
+					content += text.replace(preamble + "\r\n", "");
 					fileHandle.Close();
 				}
 				function finish() {
 					document.write(content);
-					document.execCommand('saveAs', true, '.txt');
+					document.execCommand("saveAs", true, ".txt");
 					close();
 				}
 				function fileMode() {
@@ -191,18 +193,18 @@ GOTO :EOF
 				}
 				resizeTo(0, 0);
 				for (var i = 1; i < argv.length; i++) {
-					if (argv[i].substring(0, '/D:'.length) == '/D:') {
-						directory = argv[i].replace('/D:', '').replace(/^"|"$/g, '');
-					} else if (argv[i].substring(0, '/E'.length) == '/E') {
+					if (argv[i].substring(0, "/D:".length) == "/D:") {
+						directory = argv[i].replace("/D:", "").replace(/^"|"$/g, "");
+					} else if (argv[i].substring(0, "/E".length) == "/E") {
 						eraseWhenDone = true;
-					} else if (argv[i].substring(0, '/S:'.length) == '/S:') {
-						seconds = Number(argv[i].replace('/S:', ''));
-					} else if (argv[i].substring(0, '/U'.length) == '/U') {
+					} else if (argv[i].substring(0, "/S:".length) == "/S:") {
+						seconds = Number(argv[i].replace("/S:", ""));
+					} else if (argv[i].substring(0, "/U".length) == "/U") {
 						UUIDRequested = true;
-					} else if (argv[i].substring(0, '/X:'.length) == '/X:') {
-						extension = argv[i].replace('/X:', '').replace(/^"|"$/g, '');
+					} else if (argv[i].substring(0, "/X:".length) == "/X:") {
+						extension = argv[i].replace("/X:", "").replace(/^"|"$/g, "");
 					} else {
-						fileList.push(argv[i].replace(/^"|"$/g, ''));
+						fileList.push(argv[i].replace(/^"|"$/g, ""));
 					}
 				}
 				if (UUIDRequested) {
